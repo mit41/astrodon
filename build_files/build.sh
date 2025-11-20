d095d36cb33d50ff02b2910e2c179b9c1598e53c
Michael Tal <mi-tal.dev@pm.me>
2025-11-20 17:06:10 +0100

    Prep iso (#16)
    * prepare iso file
    
    * don't install tuxedo kernel mods

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,26 +8,6 @@ ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-## Install tuxedo drivers
-# TODO: Remove this once https://github.com/ublue-os/akmods/pull/329 is merged
-# skip if we build for bazzite
-if [[ ! "${BASE_IMAGE}" =~ bazzite ]]; then
-    dnf -y copr enable gladion136/tuxedo-drivers-kmod
-    dnf -y install akmods \
-        "akmod-tuxedo-drivers-*.fc${RELEASE}.${ARCH}"
-
-    akmods --force --kernels "${KERNEL_VERSION}" --kmod "tuxedo-drivers-kmod"
-
-    for file in /usr/lib/modules/${KERNEL_VERSION}/extra/tuxedo-drivers/*.ko.xz; do
-        modinfo "$file" > /dev/null \
-        || (find /var/cache/akmods/tuxedo-drivers/ -name \*.log -print -exec cat {} \; && exit 1)
-    done
-
-    userdel akmods
-    dnf -y remove akmods
-    dnf -y copr disable gladion136/tuxedo-drivers-kmod
-fi
-
 ## Install cosmic related stuff
 dnf -y copr enable ryanabx/cosmic-epoch
 
