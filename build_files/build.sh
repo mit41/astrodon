7599f5bc11605400b3c25e507141713f69576493
Michael Tal <mi-tal.dev@pm.me>
2025-11-26 16:16:51 +0100

    disable repo after installation (#18)

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,6 +8,7 @@ ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
+# TODO: switch to official packages after beta ends
 ## Install cosmic related stuff
 dnf -y copr enable ryanabx/cosmic-epoch
 
@@ -16,3 +17,5 @@ dnf -y remove gnome-session
 
 ## Install cosmic session instead of gnome
 dnf -y install cosmic-session
+
+dnf -y copr disable ryanabx/cosmic-epoch
