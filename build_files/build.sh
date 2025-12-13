bb91a15f16692363bd179ece07ffa224aa7d955a
Michael Tal <mi-tal.dev@pm.me>
2025-12-13 18:33:27 +0100

    chore: switch to fedora packages of cosmic

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,14 +8,8 @@ ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-# TODO: switch to official packages after beta ends
-## Install cosmic related stuff
-dnf -y copr enable ryanabx/cosmic-epoch
-
-## Remove gnome session
-dnf -y remove gnome-session
+## Remove gnome stuff except for keyring libs
+dnf -y remove gnome-* --exclude gnome-keyring,gnome-keyring-pa
 
 ## Install cosmic session instead of gnome
 dnf -y install cosmic-session
-
-dnf -y copr disable ryanabx/cosmic-epoch
