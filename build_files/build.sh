d89fa810a9085d840be7dec53c712aba91aee57b
Michael Tal <mi-tal.dev@pm.me>
2025-12-17 15:16:54 +0100

    fix: typo in gnome-keyring-pam (#24)

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ RELEASE="$(rpm -E '%fedora')"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 ## Remove gnome stuff except for keyring libs
-dnf -y remove gnome-* --exclude gnome-keyring,gnome-keyring-pa
+dnf -y remove gnome-* --exclude gnome-keyring,gnome-keyring-pam
 
 ## Install cosmic session instead of gnome
 dnf -y install cosmic-session
